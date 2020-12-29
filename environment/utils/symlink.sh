4c353411ca70cdeb75e4bed21a36295b0e4764a5
Jose Angel Garrido Montoya <jgarri04@ucm.es>
2020-12-29 16:29:04 +0100

    Remove prompt menu to choose whether overwrite or not

--- environment/utils/symlink.sh
+++ environment/utils/symlink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # use BASH_SOURCE to import another scripts because this file is always imported
-path_sym="$(dirname $BASH_SOURCE)"
-source $path_sym/prompt.sh
+#path_sym="$(dirname $BASH_SOURCE)"
+#source $path_sym/prompt.sh
 
 # $1 -> source
 # $2 -> dest
@@ -14,19 +14,14 @@ create_symbolic_link(){
 
     if [ -f "$dest" ] || [ -d "$dest" ] || [ -L "$dest" ]; then
         echo "Exists a file/folder/symlink in $dest"
-        option=$(prompt_choose_option $dest)
-        if [[ $option = "yes" ]]; then
-            echo "Overwriting $dest"
-            echo "Creating symlink from $source to $dest"
-            if [[ "$root" = "yes" ]]; then
-                sudo rm "$dest"
-                sudo ln -s $source $dest
-            else
-                rm "$dest"
-                ln -s $source $dest
-            fi
+        echo "Overwriting $dest"
+        echo "Creating symlink from $source to $dest"
+        if [[ "$root" = "yes" ]]; then
+            sudo rm "$dest"
+            sudo ln -s $source $dest
         else
-            echo "$dest is not going to be overwrited."
+            rm "$dest"
+            ln -s $source $dest
         fi
     else
         echo "$dest doesnt exists. Creating the file/folder"
